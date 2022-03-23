fda2f6d81e5c99f9eb8a953d0240f9fde020f91b
Daniel Mar <mardaniel42@gmail.com>
2022-03-23 22:24:16 +1300

    Added CORS through flask-cors

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
+from flask_cors import CORS
 import app.chart_data_builder as chart_data_builder
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def homepage():
